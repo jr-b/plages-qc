d06da9ac9b963196c5fcfd10da13eca645db4fb3
jr-b <j@jrb.nz>
2024-07-07 16:58:04 -0400

    feat: add tor, scrape via ddg

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from duckduckgo_search import DDGS
+from time import sleep
+import random
 
 """
 Script pour récupérer les données concernant la qualité de l'eau des plages du Québec à partir du Programme Environnement-Plage
@@ -9,6 +12,10 @@ Source des données: https://www.environnement.gouv.qc.ca/programmes/env-plage/i
 
 Le script récupère les données de toutes les plages de chaque région du Québec et les sauvegarde dans un fichier JSON.
 """
+
+
+def sleep_random() -> None:
+    sleep(1 + 2 * random.random())
 
 
 def get_regions_ids(
@@ -23,7 +30,7 @@ def get_regions_ids(
         regions = soup.find("div", {"class": "bte-liste-region"}).find_all(
             "a", href=True
         )
-        # make tuple with (id, name)
+        # make list of tuple with (id, name)
         regions_ids = [(a["href"][-2:], a.text) for a in regions]
         return regions_ids
     except Exception as e:
@@ -39,6 +46,45 @@ def get_table(url: str) -> pd.DataFrame:
         raise ValueError("No table found")
     tables[0].columns = header
     return tables[0]
+
+
+def get_url_about_beach(searchstring: str) -> dict[str, str]:
+    """
+    Query DuckDuckGo for the first result about the beach
+    Returns a dictionary with the url of the first result, the title of the page and its description
+    """
+    sleep_random()
+    try:
+        with DDGS(proxy="socks5://127.0.0.1:9150", timeout=20) as ddgs:
+            for r in ddgs.text(
+                searchstring,
+                region="ca-fr",
+                safesearch="on",
+                max_results=1,
+            ):
+                return r
+    except Exception as e:
+        print(f"Error getting url: {e}")
+        return {}
+
+
+def get_image_about_beach(searchstring: str) -> dict[str, str]:
+    """
+    Query DuckDuckGo for the first image about the beach
+    """
+    sleep_random()
+    try:
+        with DDGS(proxy="socks5://127.0.0.1:9150", timeout=20) as ddgs:
+            for r in ddgs.images(
+                searchstring,
+                region="ca-fr",
+                safesearch="on",
+                max_results=1,
+            ):
+                return r
+    except Exception as e:
+        print(f"Error getting image: {e}")
+        return {}
 
 
 def main():
@@ -57,6 +103,19 @@ def main():
             continue
     # Concatenate all the tables
     df = pd.concat(alltables)
+    count = len(df)
+    for i, row in df.iterrows():
+        print("Processing", i, "of", count)
+        searchstring = f"{row['plagename']} {row['municipalite']} Québec"
+        # Get the url of the first result
+        result = get_url_about_beach(searchstring)
+        if result:
+            df.loc[i, "remotecontent"] = result["href"]
+        # Get the image of the first result
+        result = get_image_about_beach(searchstring)
+        if result:
+            df.loc[i, "image"] = result["image"]
+
     df.to_json("plages.json", orient="records", force_ascii=False)
 
 
